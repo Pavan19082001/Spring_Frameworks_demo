f6b3262503c1a27d74510bc6119cb3810eb11642
Pavan19082001 <63573888+Pavan19082001@users.noreply.github.com>
2022-03-18 11:46:03 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,21 @@
+package com;
+
+public class Customer {
+	private String customerName;
+	private String customerEmail;
+	public Customer() {}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+	
+
+}
---
+++ Person.java
@@ -0,0 +1,22 @@
+package com;
+
+public class Person {
+	private String personName;
+	private String personAddress;
+	public Person() {}
+	public String getPersonName() {
+		return personName;
+	}
+	
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+	public String getPersonAddress() {
+		return personAddress;
+	}
+	public void setPersonAddress(String personAddress) {
+		this.personAddress = personAddress;
+	}
+	
+
+}
